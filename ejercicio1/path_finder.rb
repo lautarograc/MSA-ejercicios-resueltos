1d887d8eb89832c3ecb1f99bd7468c6aecf4a490
lautarograciani <lautaro@graciani.ar>
2023-04-03 19:52:17 -0300

    changed implementation recursion to iteration

--- ejercicio1/path_finder.rb
+++ ejercicio1/path_finder.rb
@@ -2,6 +2,7 @@ class PathFinder
   def initialize(map)
     @map = map
   end
+
   def find_longest_path
     max_path_length = 0
     max_path_item = ""
@@ -10,8 +11,30 @@ class PathFinder
     @map.each_with_index do |row, i|
       row.each_with_index do |item, j|
         if !@visited[i][j]
+          stack = [[i, j]]
           path = []
-          create_path(i, j, path)
+
+          while !stack.empty?
+            curr_i, curr_j = stack.pop
+            if !@visited[curr_i][curr_j]
+              @visited[curr_i][curr_j] = true
+              path << @map[curr_i][curr_j]
+              adjacent_tiles = [
+                [curr_i - 1, curr_j],
+                [curr_i, curr_j - 1],
+                [curr_i + 1, curr_j],
+                [curr_i, curr_j + 1]
+              ]
+              adjacent_tiles.each do |row, col|
+                if row >= 0 && col >= 0 && row < @map.length &&
+                     col < @map[0].length &&
+                     @map[row][col] == @map[curr_i][curr_j] &&
+                     !@visited[row][col]
+                  stack << [row, col]
+                end
+              end
+            end
+          end
 
           if path.length > max_path_length
             max_path_length = path.length
@@ -22,18 +45,5 @@ class PathFinder
     end
 
     return "#{max_path_item} #{max_path_length}"
-  end
-  def create_path(i, j, path)
-    @visited[i][j] = true
-    path << @map[i][j]
-
-    adjacent_tiles = [[i - 1, j], [i, j - 1], [i + 1, j], [i, j + 1]]
-
-    adjacent_tiles.each do |row, col|
-      if row >= 0 && col >= 0 && row < @map.length && col < @map[0].length &&
-           @map[row][col] == @map[i][j] && !@visited[row][col]
-        create_path(row, col, path)
-      end
-    end
   end
 end
