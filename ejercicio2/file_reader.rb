a69b8fa86751a8b0eeeda8109911cf58540c72d0
lautarograciani <lautaro@graciani.ar>
2023-04-03 18:26:17 -0300

    added number to output

--- ejercicio2/file_reader.rb
+++ ejercicio2/file_reader.rb
@@ -1,14 +1,16 @@
 class FileReader
-    def self.read_numbers(file_path)
-      numbers = []
-      File.open(file_path).each do |line|
+  def self.read_cases(file_path)
+    cases = []
+    current_case = nil
+    File
+      .open(file_path)
+      .each do |line|
         if line.start_with?("#")
-          next
+          current_case = line[/\d+/].to_i
         elsif !line.strip.empty?
-          numbers << line.strip.to_i
+          cases << [current_case, line.strip.to_i]
         end
       end
-      numbers
-    end
+    cases
   end
-  
+end
--- ejercicio2/main.rb
+++ ejercicio2/main.rb
@@ -1,14 +1,14 @@
 require_relative "yaml_loader"
 require_relative "file_reader"
 require_relative "number_converter"
-
 def main
-  data = YamlLoader.load("numbers.yml")
+  data = YamlLoader.load("ejercicio2/numbers.yml")
   input_file = ARGV[0]
   raise "Usage: ruby main.rb <numbers_file>" if input_file.nil?
-  raise "File not found: #{numbers_file}" unless File.exist?(input_file)
-  numbers = FileReader.read_numbers(input_file)
-  numbers.each { |number| puts NumberConverter.new(data).to_words(number) }
+  raise "File not found: #{input_file}" unless File.exist?(input_file)
+  cases = FileReader.read_cases(input_file)
+  cases.each do |current_case, number|
+    puts "#{current_case}. #{NumberConverter.new(data).to_words(number)}"
+  end
 end
-
 main
